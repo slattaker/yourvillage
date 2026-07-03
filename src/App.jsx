1292ea44b63e300e4be31a63e365282b435be38c
Sean Lattaker <sean.lattaker@sparknav.com>
2026-07-02 22:13:01 -0400

    Initial YourVillage website

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <h1>Your Village</h1>
+      <h2>It Takes a Village.</h2>
+
+      <p>
+        Compassionate birth and postpartum support for growing families.
+      </p>
+
+      <button>Schedule a Consultation</button>
+    </div>
+  );
+}
+
+export default App;
